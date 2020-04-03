1fd115aacd497c1ccde59312ac584297804b671e
Mugenzijmv <56726317+Mugenzijmv@users.noreply.github.com>
2020-04-03 02:27:16 +0200

    Add files via upload

---
+++ satina/header.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+        <div class="collapse top-search" id="collapseExample">
+            <div class="card card-block">
+                <div class="newsletter-widget text-center">
+                    <form class="form-inline">
+                        <input type="text" class="form-control" placeholder="What you are looking for?">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div><!-- end newsletter -->
+            </div>
+        </div><!-- end top-search -->
+
+        <div class="topbar-section" style="background-color: #1683b3;background-image: url('images/footer.png');border-top: 4px dashed #fff;">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6 hidden-xs-down">
+                        <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus" style="color: #fff;"></i></a>
+                        </div><!-- end social -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-6 hidden-md-down">
+                        <div class="topmenu text-center" >
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><a  href="blog-category-01.html"><i style="color: #fff;" class="fa fa-envelope-o"></i> <strong style="color: #fff;"> Email: info@satina-holding.com</strong></a></li>
+                                <li class="list-inline-item"><a href="blog-category-02.html"><i style="color: #fff;" class="fa fa-phone"></i> <strong style="color: #fff;">628 44 95 77/624 20 20 99</strong></a></li>
+                            </ul><!-- end ul -->
+                        </div><!-- end topmenu -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                        <div class="topsearch text-right">
+                            <a data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><i style="color: #fff;" class="fa fa-search"></i> <strong style="color: #fff;">Search here</strong></a>
+                        </div><!-- end search -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end header-logo -->
+        </div><!-- end topbar -->
+
+        <header class="header">
+            <div class="container">
+                <div class="col-lg-12 col-xm-12">
+                <nav class="navbar navbar-inverse navbar-toggleable-md">
+                    <div class="row">
+                      
+                        <div class="col-xs-4 col-xs-4">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    
+                            <button st  class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cloapediamenu" aria-controls="cloapediamenu" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+
+                                </div>
+                                <div class="col-md-6">
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-12 col-md-12">
+                            
+                    <div class="collapse navbar-collapse justify-content-md-center" id="cloapediamenu" style="width: 100%;">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Accueil</a>
+                            </li>
+
+
+                            <li class="nav-item dropdown has-submenu">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">À propos de nous</a>
+                                <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                                    <li><a class="dropdown-item" href="mission.php">Mission & Vision</a></li>
+                                    <li><a class="dropdown-item" href="#">Comité executive</a></li>
+                                </ul>
+                            </li>
+
+
+                            <li class="nav-item dropdown has-submenu">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Nos produits</a>
+                                <ul class="dropdown-menu" aria-labelledby="dropdown02">
+                                    <li><a class="dropdown-item" href="telephones.php">Telephones</a></li>
+                                    <li><a class="dropdown-item" href="accessoires.php">Accessoires</a></li>
+                                    <li><a class="dropdown-item" href="#">My cash</a></li>
+                                </ul>
+                            </li>
+
+
+                            <li class="nav-item hidden-md-down hidden-sm-down hidden-xs-down">
+
+                            <div class="logo">
+                            <a href="index.php" ><img style="width: 50%;" src="images/logo-satina.png" alt="Satina logo"></a>
+                        </div><!-- end logo -->
+
+                    </li>
+
+
+                            <li class="nav-item dropdown has-submenu menu-large ">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Boutique</a>
+                                <ul class="dropdown-menu megamenu" aria-labelledby="dropdown01">
+                                    <li>
+                                        <div class="mega-menu-content clearfix">
+                                            <div class="tab-details clearfix" style="background-color: #f2f2f2;">
+                                                <div id="cat05" class="tabcontent active text-center">
+                                                    <div class="row" >
+                                                        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                                                            <div class="blog-box">
+                                                                <div class="post-media">
+                                                                    <a href="single.html" title="">
+                                                                        <img src="images/logos/felux.png" alt="" class="img-fluid">
+                                                                    </a>
+                                                                </div><!-- end media -->
+                                                            </div><!-- end blog-box -->
+                                                        </div>
+
+                                                        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                                                            <div class="blog-box">
+                                                                <div class="post-media">
+                                                                    <a href="single.html" title="">
+                                                                        <img src="images/logos/oraimo.png" alt="" class="img-fluid">
+                                                                    </a>
+                                                                </div><!-- end media -->
+                                                            </div><!-- end blog-box -->
+                                                        </div>
+
+                                                        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                                                            <div class="blog-box">
+                                                                <div class="post-media">
+                                                                    <a href="single.html" title="">
+                                                                        <img src="images/logos/lava.png" alt="" class="img-fluid">
+                                                                    </a>
+                                                                </div><!-- end media -->
+                                                            </div><!-- end blog-box -->
+                                                        </div>
+
+                                                        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                                                            <div class="blog-box">
+                                                                <div class="post-media">
+                                                                    <a href="#" title="">
+                                                                        <img src="images/logos/samsung.png" alt="" class="img-fluid">
+                                                                    </a>
+                                                                </div><!-- end media -->
+                                                            </div><!-- end blog-box -->
+                                                        </div>
+
+                                                    </div><!-- end row -->
+                                                </div>
+                                            </div><!-- end tab-details -->
+                                        </div><!-- end mega-menu-content -->
+                                    </li>
+                                </ul>
+                            </li>
+
+
+                            <li class="nav-item dropdown has-submenu">
+                                <a class="nav-link" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
+                            </li>
+                            <li class="nav-item dropdown has-submenu">
+                                <a class="nav-link" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact</a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                        </div>
+                    </div>
+
+                </nav>
+            </div>
+            </div><!-- end container -->
+        </header><!-- end header -->
+</body>
+</html>
---
+++ satina/notre-equipe.php
@@ -0,0 +1,176 @@
+Notre équipe<div class="container">
+    <div class="row">
+        <div class="container-fluid padding" >
+            <div class="row celcome text-center">
+                <div class="col-12">
+                    <h3 class="display-4">Notre équipe</h3>
+                </div>
+                <hr>
+                <div class="col-12">
+                    <p class="lead">SATINA distributeur officiel des produits Samsung, Lava, Oraimo, Felux en guinée</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/800.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">DG (Mohamed Lamine DIALLO)</a>
+                    </div>
+                    <div class="desc">Directeur Général, Associé</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                </div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/8002.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">DGA (Mamadou Saliou Boussoura DIALLO)</a>
+                    </div>
+                    <div class="desc">Directeur Général Adjoint, Associé</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/8003.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">TG (Mamadou Oury SOW)</a>
+                    </div>
+                    <div class="desc">Trésorier Général, Associé</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                </div>
+            </div>
+
+        </div>
+
+
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/800.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">DCM (Mamadou DIAO BAH)</a>
+                    </div>
+                    <div class="desc">Directeur Commercial et Marketing</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                </div>
+            </div>
+
+        </div>
+
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/8002.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">DAF (Mr Saifoulaye Toure)</a>
+                    </div>
+                    <div class="desc">Directeur Administratif et Financier</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                </div>
+            </div>
+
+        </div>
+
+
+        <div class="col-lg-4 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="images/800.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="">DRH (Aïssata Sabana DIALLO)</a>
+                    </div>
+                    <div class="desc">Directrice des Ressources Humaines</div>
+                </div>
+                <div class="bottom">
+                         <div class="topsocial">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                        </div><!-- end social -->                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
